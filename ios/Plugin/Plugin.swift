5052c6fb4ed5d6329215976dab2a735a852d52c9
Dirk Gausmann <di.gaus@gmx.de>
2021-08-17 13:31:16 -0700

    Make it able to use a VPN and get the IP

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -201,15 +201,13 @@ public class Wifi: CAPPlugin {
         for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
             let interface = ifptr.pointee
 
-            // Check for IPv4 or IPv6 interface:
+            // Check for IPv4:
             let addrFamily = interface.ifa_addr.pointee.sa_family
-            //|| addrFamily == UInt8(AF_INET6) 
+            // addrFamily == UInt8(AF_INET6)
             if addrFamily == UInt8(AF_INET) {
-
                 // Check interface name:
                 let name = String(cString: interface.ifa_name)
-                if  name == "en0" {
-
+                if  name == "en0" || name.starts(with: "tap") || name.starts(with: "ppp") || name.starts(with: "ipsec") || name.starts(with: "utun") {
                     // Convert interface address to a human readable string:
                     var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
                     getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
