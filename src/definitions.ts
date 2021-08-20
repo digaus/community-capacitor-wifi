ea60ebaa7cad4ef6ef199c42270000beae5e3d3b
Dirk Gausmann <di.gaus@gmx.de>
2021-08-20 16:32:06 -0700

    Fix Defenitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,15 +1,19 @@
 export interface WifiPlugin {
   getIP(): Promise<{ ip: string | null }>;
   getSSID(): Promise<{ ssid: string | null }>;
-  connect(options: IWifiConnectOptions): Promise<{ ssid: string | null }>;
-  connectPrefix(options: IWifiConnectOptions): Promise<{ ssid: string | null }>;
+  connect(options: {
+    ssid: string,
+    password?: string,
+    /** iOS only: https://developer.apple.com/documentation/networkextension/nehotspotconfiguration/2887518-joinonce */
+    joinOnce?: boolean,
+    /** Android only: https://developer.android.com/reference/android/net/wifi/WifiNetworkSpecifier.Builder#setIsHiddenSsid(boolean) */
+    isHiddenSsid?: boolean,
+  }): Promise<{ ssid: string | null }>;
+  connectPrefix(options: {
+    ssid: string,
+    password?: string,
+     /** iOS only: https://developer.apple.com/documentation/networkextension/nehotspotconfiguration/2887518-joinonce */
+    joinOnce?: boolean,
+  }): Promise<{ ssid: string | null }>;
   disconnect(): Promise<void>;
-}
-export interface IWifiConnectOptions {
-  ssid: string;
-  password?: string;
-  /** iOS only: https://developer.apple.com/documentation/networkextension/nehotspotconfiguration/2887518-joinonce */
-  joinOnce?: boolean;
-  /** Android only: https://developer.android.com/reference/android/net/wifi/WifiNetworkSpecifier.Builder#setIsHiddenSsid(boolean) */
-  isHiddenSsid?: boolean;
 }
